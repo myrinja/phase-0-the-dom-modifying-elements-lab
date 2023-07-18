058c87b5770af8f342ab960775124a00469073e9
myrinja <myrinjau98@gmail.com>
2023-07-18 23:24:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
+
+
+
